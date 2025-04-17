1ef851f9c486eab2d149a03dfd387fea2e33d309
ClaudioBaptista <claudio.baptista@etec.sp.gov.br>
2025-04-16 22:01:15 -0300

    Projeto parte 2

--- PrjTema/App.xaml.cs
+++ PrjTema/App.xaml.cs
@@ -1,15 +1,48 @@
-﻿namespace PrjTema
+﻿using PrjTema.Resources.Theme; // Supondo que DarkTheme e LightTheme são definidos em arquivos .xaml
+using System.Collections.ObjectModel; // Para ICollection
+
+namespace PrjTema
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            InitTheme();
+            MainPage = new AppShell();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
+        private void InitTheme()
         {
-            return new Window(new AppShell());
+            Application.Current.UserAppTheme = AppTheme.Dark;  // Corrigido de AppAction.Current para Application.Current
+            Application.Current.Resources.MergedDictionaries.Add(new DarkTheme());
         }
+
+        public static AppTheme ChangeTheme()
+        {
+            ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+
+            // Alternar entre temas Claro e Escuro
+            if (Application.Current.UserAppTheme == AppTheme.Dark)
+            {
+                Application.Current.UserAppTheme = AppTheme.Light;
+                mergedDictionaries.Remove(new DarkTheme()); // Remover o tema escuro
+                mergedDictionaries.Add(new LightTheme());  // Adicionar o tema claro
+            }
+            else
+            {
+                Application.Current.UserAppTheme = AppTheme.Dark;
+                mergedDictionaries.Remove(new LightTheme()); // Remover o tema claro
+                mergedDictionaries.Add(new DarkTheme());  // Adicionar o tema escuro
+            }
+
+            // Notificar que o tema mudou
+            ThemeChanged?.Invoke(Application.Current.UserAppTheme);
+            return Application.Current.UserAppTheme;
+        }
+
+        // Event handler para notificação de mudança de tema
+        public static event Action<AppTheme> ThemeChanged;
     }
 }
--- PrjTema/Resources/Theme/DarkTheme.xaml.cs
+++ PrjTema/Resources/Theme/DarkTheme.xaml.cs
@@ -2,8 +2,8 @@ namespace PrjTema.Resources.Theme;
 
 public partial class DarkTheme : ResourceDictionary
 {
-	public DarkTheme()
-	{
-		InitializeComponent();
-	}
+    public DarkTheme()
+    {
+        InitializeComponent(); //funciona pois está vinculado ao XAML com x:Class
+    }
 }
